7257ebf0b5dbc63a3bcb446632b45c74dad46fb2
Christiaan Bakker <contact@anicator.com>
2013-11-01 17:20:02 +0100

    Added various non-working buttons to main UI. Started working on button input handling.

---
+++ Calculator/CalculatorWindow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorWindow : Form
+    {
+        private string m_InputString = "0";
+        public CalculatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetInputString()
+        {
+            m_InputString = "0";
+            calcResultLabel.Text = m_InputString;
+            calcResultLabel.Location = new Point(503, 51);
+        }
+
+        private void AddToInputString(string n)
+        {
+            m_InputString += n;
+            calcResultLabel.Text = m_InputString;
+            int offsetLabelBy = (m_InputString.Length - 1) * 14;
+            calcResultLabel.Location = new Point(503 - offsetLabelBy, 51);
+        }
+
+        private void buttonZero_Click(object sender, EventArgs e)
+        {
+            AddToInputString("0");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ResetInputString();
+        }
+    }
+}
